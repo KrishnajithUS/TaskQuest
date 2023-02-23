f7a690ed7cdca1ae96f7b533165928f2c43d74c7
Ubuntu <ubuntu@ip-172-31-14-174.ec2.internal>
2023-02-23 18:32:28 +0000

    deployment completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR,'frontend/build/static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 MEDIA_URL = '/media/'
